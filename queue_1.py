6acf3aa193c5d9da1287880f8d31e087e3e37e23
nas160sa <nasser.abo.sadem@gmail.com>
2022-12-26 17:27:32 +0300

    add a new file queue1.py

--- queue_1.py
+++ queue_1.py
@@ -1,21 +1,15 @@
-"""Make a Queue class using a list!
-Hint: You can use any Python list method
-you'd like! Try to write each one in as 
-few lines as possible.
-Make sure you pass the test cases too!"""
-
 class Queue:
     def __init__(self, head=None):
         self.storage = [head]
 
     def enqueue(self, new_element):
-        pass
+        self.storage.append(new_element)
 
     def peek(self):
-        pass 
+        return self.storage[0] 
 
     def dequeue(self):
-        pass
+        return self.storage.pop(0)
     
 # Setup
 q = Queue(1)
