4d2884b49065b787f365dfb15d6145d1e55f2a30
nas160sa <nasser.abo.sadem@gmail.com>
2022-12-28 19:05:06 +0300

    edit selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,12 +1,10 @@
 def selection_sort(arr):
-    for i in range(len(arr) - 1):
+    for i in range(len(arr)):
         min = i
         for j in range(i + 1, len(arr)):
             if arr[min] > arr[j]:
                 min = j
-        temp = arr[i]
-        arr[i] = arr[min]
-        arr[min] = temp
+        arr[i], arr[min] = arr[min], arr[i]
 
     return arr
 
