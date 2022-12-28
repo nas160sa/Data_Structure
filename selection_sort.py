dd61c57363edd17eea2feb193ae474150b263ecf
nas160sa <nasser.abo.sadem@gmail.com>
2022-12-28 18:51:20 +0300

    done

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,16 @@
-def find_smallest(arr):
-    pass
-
 def selection_sort(arr):
-    pass
+    for i in range(len(arr) - 1):
+        min = i
+        for j in range(i + 1, len(arr)):
+            if arr[min] > arr[j]:
+                min = j
+        temp = arr[i]
+        arr[i] = arr[min]
+        arr[min] = temp
+
+    return arr
+
+
+#Test
+arr = [5,9,4,2,8,6,1,7,3,0]
+print(selection_sort(arr)) 
